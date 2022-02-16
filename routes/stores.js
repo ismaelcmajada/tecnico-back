20e9fa5632fac9ee91199a3f27da956a635815c3
unknown <ismaelcmajada@gmail.com>
2022-02-16 14:12:07 +0000

    Arreglo en status de respuesta, arreglo en 'stores'.

--- routes/stores.js
+++ routes/stores.js
@@ -3,7 +3,7 @@ const verify = require('./verifyToken')
 
 router.get('/', verify, (req, res) => {
     res.json({
-        posts: {
+        stores: {
             title: 'PCBox Puerto del Rosario', 
             description: 'Tienda de informática y reparación de equipos.'
         }
